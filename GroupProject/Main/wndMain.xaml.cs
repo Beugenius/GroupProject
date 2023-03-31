89679d3ad6fd20d904f16e87e15cb71572f3fcd3
Bryce Hutchinson <brycehutchinson@mail.weber.edu>
2023-03-31 13:34:05 -0600

    Main window opens search window on search click

--- GroupProject/Main/wndMain.xaml.cs
+++ GroupProject/Main/wndMain.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GroupProject.Search;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,15 @@ namespace GroupProject.Main
 
         private void SearchMenuItem_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                wndSearch SearchWindow = new();
+                SearchWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something broke!"); 
+            }
             // pass the selected invoice id as a reference during window creation
             // set referenced value in search window so when that window closes, the value is already here and ready to use 
             // reload main window with selected invoice id regardless if one was actually selected or not 
